d8df48caef786cb7ee352f7f07c2000e87d1a593
kwguo <shana0440@gmail.com>
2018-12-23 01:00:33 +0800

    remove unused file

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import { createServer } from "bottender/express";
-import { BotServer } from "./Bot";
-
-const server = createServer(BotServer);
-
-server.listen(8080, () => {
-  console.log("server is running on 8080 port...");
-});
