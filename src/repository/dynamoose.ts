c4a660f61a67af5c6c4c944151c522b50d201e3d
kwguo <shana0440@gmail.com>
2018-12-25 21:35:41 +0800

    use aws default configuration

--- src/repository/dynamoose.ts
+++ src/repository/dynamoose.ts
@@ -3,12 +3,6 @@ import config from "../config";
 
 if (config.app.isOffline) {
   dynamoose.local("http://localhost:8000");
-} else {
-  dynamoose.AWS.config.update({
-    accessKeyId: config.aws.accessKeyId,
-    secretAccessKey: config.aws.secretAccessKey,
-    region: config.aws.region
-  });
 }
 
 export default dynamoose;
