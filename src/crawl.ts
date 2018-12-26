2dccc2d5502c0db6f0769fde9a73a2139d0f3396
kwguo <shana0440@gmail.com>
2018-12-26 10:00:07 +0800

    launch more lambda to crawl data parallely

--- src/crawl.ts
+++ src/crawl.ts
@@ -1,4 +1,5 @@
 import { Handler } from "aws-lambda";
+import * as AWS from "aws-sdk";
 import * as Bot from "./Bot";
 import Crawler from "./crawlers/Crawler";
 import GoodSmileCrawler from "./crawlers/GoodSmileCrawler";
@@ -11,42 +12,70 @@ import AlphamaxCrawler from "./crawlers/AlphamaxCrawler";
 import PulchraCrawler from "./crawlers/PulchraCrawler";
 import { leaveUnsavedURL, saveFigures } from "./repository/figure";
 import { validateFigure } from "./validators/figure";
+import { getFunctionName } from "./utils/function";
 
-class CrawlFlow {
-  crawlers: Crawler[] = [];
-  constructor() {
-    this.crawlers.push(new GoodSmileCrawler());
-    this.crawlers.push(new AlterCrawler());
-    this.crawlers.push(new KotobukiyaCrawler());
-    this.crawlers.push(new TokyofigureCrawler());
-    this.crawlers.push(new FnexCrawler());
-    this.crawlers.push(new AoshimaCrawler());
-    this.crawlers.push(new AlphamaxCrawler());
-    this.crawlers.push(new PulchraCrawler());
-  }
-
-  async start() {
-    for (const crawler of this.crawlers) {
-      const urls: string[] = await crawler.getFiguresURL();
-      const unsavedURLs = await leaveUnsavedURL(urls);
-      const figures = await Promise.all(unsavedURLs.map(crawler.getFigure));
-      const { validated, invalidated } = validateFigure(figures);
-      // TODO: alert invalidated figures
-      await Bot.multicastFigures(validated);
-      await saveFigures(validated);
-    }
-  }
+interface CrawlPageEvent {
+  crawler: string;
 }
 
+interface CrawlFigureEvent {
+  crawler: string;
+  url: string;
+}
+
+const lambda = new AWS.Lambda();
+
+const crawlers: { [key: string]: Crawler } = {
+  GoodSmileCrawler: new GoodSmileCrawler(),
+  AlterCrawler: new AlterCrawler(),
+  KotobukiyaCrawler: new KotobukiyaCrawler(),
+  TokyofigureCrawler: new TokyofigureCrawler(),
+  FnexCrawler: new FnexCrawler(),
+  AoshimaCrawler: new AoshimaCrawler(),
+  AlphamaxCrawler: new AlphamaxCrawler(),
+  PulchraCrawler: new PulchraCrawler()
+};
+
+const invokeLambda = (
+  fn: string,
+  payload: CrawlPageEvent | CrawlFigureEvent
+) => {
+  return lambda
+    .invoke({
+      FunctionName: fn,
+      InvocationType: "Event",
+      Payload: JSON.stringify(payload)
+    })
+    .promise();
+};
+
 export const handler: Handler = async () => {
-  try {
-    // TODO: start another lambda to parse figure
-    const crawl = new CrawlFlow();
-    await crawl.start();
-    process.exit();
-  } catch (err) {
-    // TODO handler error
-    console.log("err", err);
-    process.exit();
-  }
+  const invoke = Object.keys(crawlers).map(crawler => {
+    return invokeLambda(getFunctionName("CrawlPage"), {
+      crawler
+    });
+  });
+  return await Promise.all(invoke);
+};
+
+export const handlePage: Handler = async (event: CrawlPageEvent) => {
+  const crawler: Crawler = crawlers[event.crawler];
+  const urls: string[] = await crawler.getFiguresURL();
+  const unsavedURLs = await leaveUnsavedURL(urls);
+  const invoke = unsavedURLs.map(url => {
+    return invokeLambda(getFunctionName("CrawlFigure"), {
+      crawler: event.crawler,
+      url
+    });
+  });
+  return await Promise.all(invoke);
+};
+
+export const handleFigure: Handler = async (event: CrawlFigureEvent) => {
+  const crawler: Crawler = crawlers[event.crawler];
+  const figure = await crawler.getFigure(event.url);
+  const { validated, invalidated } = validateFigure([figure]);
+  // TODO: alert invalidated figures
+  await Bot.multicastFigures(validated);
+  return await saveFigures(validated);
 };
