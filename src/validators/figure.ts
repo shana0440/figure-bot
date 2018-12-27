782fe5722bc3428ba688a74f2d3659c72c3d1bb9
kwguo <shana0440@gmail.com>
2018-12-27 10:31:08 +0800

    update validate of figure

--- src/validators/figure.ts
+++ src/validators/figure.ts
@@ -1,9 +1,38 @@
 import { IFigure } from "../models/figure";
+import * as _ from "lodash";
+
+const validate = (figure: IFigure): boolean => {
+  if (figure.id === "") {
+    return false;
+  }
+
+  if (figure.name === "") {
+    return false;
+  }
+
+  if (figure.company === "") {
+    return false;
+  }
+
+  if (isNaN(parseInt(figure.price))) {
+    return false;
+  }
+
+  if (!_.startsWith(figure.image, "https://")) {
+    return false;
+  }
+
+  if (isNaN(figure.releaseDate.getTime())) {
+    return false;
+  }
+
+  return true;
+};
 
 export const validateFigure = (
   figures: IFigure[]
 ): { validated: IFigure[]; invalidated: IFigure[] } => {
-  const validated = figures.filter(figure => figure.name !== "");
-  const invalidated = figures.filter(figure => figure.name === "");
+  const validated = figures.filter(figure => validate(figure));
+  const invalidated = figures.filter(figure => !validate(figure));
   return { validated, invalidated };
 };
