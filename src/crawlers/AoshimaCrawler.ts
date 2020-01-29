fb7cf781cd5ae83eadb20b42021051008f688808
kwguo <shana0440@gmail.com>
2020-01-29 17:05:14 +0800

    aoshima: fix url & fetch one by one

--- src/crawlers/AoshimaCrawler.ts
+++ src/crawlers/AoshimaCrawler.ts
@@ -1,5 +1,5 @@
-import { forkJoin } from 'rxjs';
-import { mergeMap, map } from 'rxjs/operators';
+import { queueScheduler, Observable } from 'rxjs';
+import { mergeMap, map, reduce, mergeAll, observeOn } from 'rxjs/operators';
 
 import { FigureCrawler } from './FigureCrawler';
 import { Request } from '../request/Request';
@@ -14,34 +14,38 @@ export class AoshimaCrawler implements FigureCrawler {
 
   async fetchFigures() {
     return this.request
-      .request('http://www.aoshima-bk.co.jp/product/?s=&brand=&category=フィギュア')
+      .request('http://www.aoshima-bk.co.jp/product/?s=&brand=&category=%E3%83%95%E3%82%A3%E3%82%AE%E3%83%A5%E3%82%A2')
       .pipe(
-        mergeMap(($) => {
+        map(($) => {
           const links = $('.item-list > li > a:nth-child(1)')
             .map((i, it) => $(it).attr('href'))
             .get();
-          return forkJoin(links.map((it) => this.request.request(it).pipe(map((x) => [it, x]))));
+          return links;
         }),
-        map(($figures) => {
-          return $figures.map(([url, $]) => {
-            const name = $('.mainContents > h1:nth-child(2)').text();
-            const cover = $('.img > img:nth-child(1)')
-              .attr('src')
-              .replace('http', 'https');
-            const price = $('.itemData > dl:nth-child(2) > dd:nth-child(10)').text();
-            const publishAt = $('.itemData > dl:nth-child(2) > dd:nth-child(8)')
-              .text()
-              .trim();
-            const figure: Figure = {
-              url,
-              name,
-              cover,
-              price,
-              publishAt,
-            };
-            return figure;
-          });
-        })
+        mergeAll<string>(),
+        mergeMap<string, Observable<[string, CheerioStatic]>>((url) =>
+          this.request.request(url).pipe(map(($) => [url, $]))
+        ),
+        observeOn(queueScheduler),
+        map(([url, $]) => {
+          const name = $('.mainContents > h1:nth-child(2)').text();
+	  // FIXME: should show not found image
+          const coverSrc = $('.img > img:nth-child(1)').attr('src') || '';
+          const cover = coverSrc.replace('http', 'https');
+          const price = $('.itemData > dl:nth-child(2) > dd:nth-child(10)').text();
+          const publishAt = $('.itemData > dl:nth-child(2) > dd:nth-child(8)')
+            .text()
+            .trim();
+          const figure: Figure = {
+            url,
+            name,
+            cover,
+            price,
+            publishAt,
+          };
+          return figure;
+        }),
+        reduce<Figure, Figure[]>((acc, it) => [...acc, it], [])
       )
       .toPromise();
   }
