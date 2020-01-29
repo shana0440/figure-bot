cc2a6be3b0dabb8e4c610e8a5edabd067280b04c
kwguo <shana0440@gmail.com>
2020-01-29 17:23:35 +0800

    kotobukiya: fetch one by one

--- src/crawlers/KotobukiyaCrawler.ts
+++ src/crawlers/KotobukiyaCrawler.ts
@@ -1,5 +1,5 @@
-import { forkJoin } from 'rxjs';
-import { mergeMap, map } from 'rxjs/operators';
+import { Observable, queueScheduler } from 'rxjs';
+import { mergeMap, map, mergeAll, reduce, observeOn } from 'rxjs/operators';
 
 import { FigureCrawler } from './FigureCrawler';
 import { Request } from '../request/Request';
@@ -17,29 +17,32 @@ export class KotobukiyaCrawler implements FigureCrawler {
     return this.request
       .request(`${host}/product-category/figure/`)
       .pipe(
-        mergeMap(($) => {
+        map(($) => {
           const links = $('.product-item')
             .map((i, it) => host + $(it).attr('href'))
             .get();
-
-          return forkJoin(links.map((it) => this.request.request(it).pipe(map((x) => [it, x]))));
+          return links;
         }),
-        map(($figures) => {
-          return $figures.map(([url, $]) => {
-            const name = $('.product-title h1').text();
-            const cover = host + $('.product-main img').attr('src');
-            const price = $('.product-data dl dd:nth-child(12)').text();
-            const publishAt = $('.product-data dl dd:nth-child(6)').text();
-            const figure: Figure = {
-              url,
-              name,
-              cover,
-              price,
-              publishAt,
-            };
-            return figure;
-          });
-        })
+        mergeAll<string>(),
+        mergeMap<string, Observable<[string, CheerioStatic]>>((url) =>
+          this.request.request(url).pipe(map(($) => [url, $]))
+        ),
+        observeOn(queueScheduler),
+        map(([url, $]) => {
+          const name = $('.product-title h1').text();
+          const cover = host + $('.product-main img').attr('src');
+          const price = $('.product-data dl dd:nth-child(12)').text();
+          const publishAt = $('.product-data dl dd:nth-child(6)').text();
+          const figure: Figure = {
+            url,
+            name,
+            cover,
+            price,
+            publishAt,
+          };
+          return figure;
+        }),
+        reduce<Figure, Figure[]>((acc, it) => [...acc, it], [])
       )
       .toPromise();
   }
