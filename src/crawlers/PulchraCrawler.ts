0ea5d1f369503340a1645e5631ecd180ffea8176
kwguo <shana0440@gmail.com>
2020-01-29 17:30:37 +0800

    pulchra: fetch one by one

--- src/crawlers/PulchraCrawler.ts
+++ src/crawlers/PulchraCrawler.ts
@@ -1,5 +1,5 @@
-import { forkJoin } from 'rxjs';
-import { mergeMap, map } from 'rxjs/operators';
+import { Observable, queueScheduler } from 'rxjs';
+import { mergeMap, map, mergeAll, reduce, observeOn } from 'rxjs/operators';
 
 import { FigureCrawler } from './FigureCrawler';
 import { Request } from '../request/Request';
@@ -17,31 +17,34 @@ export class PulchraCrawler implements FigureCrawler {
     return this.request
       .request(`${host}/category/select/cid/312/page/1/mode/2/language/ja`)
       .pipe(
-        mergeMap(($) => {
+        map(($) => {
           const links = $('.item .image > a')
             .map((i, it) => $(it).attr('href'))
             .get();
-
-          return forkJoin(links.map((it) => this.request.request(it).pipe(map((x) => [it, x]))));
+          return links;
         }),
-        map(($figures) => {
-          return $figures.map(([url, $]) => {
-            const name = $('div.title:nth-child(1) > h1:nth-child(1)').text();
-            const cover = `${host}/` + $('#zoom_09').attr('src');
-            const price = $('.price > h3').text();
-            const publishAt = $('.description')
-              .text()
-              .match(/【発売日】(.*)\B/)[1];
-            const figure: Figure = {
-              url,
-              name,
-              cover,
-              price,
-              publishAt,
-            };
-            return figure;
-          });
-        })
+        mergeAll<string>(),
+        mergeMap<string, Observable<[string, CheerioStatic]>>((url) =>
+          this.request.request(url).pipe(map(($) => [url, $]))
+        ),
+        observeOn(queueScheduler),
+        map(([url, $]) => {
+          const name = $('div.title:nth-child(1) > h1:nth-child(1)').text();
+          const cover = `${host}/` + $('#zoom_09').attr('src');
+          const price = $('.price > h3').text();
+          const description = $('.description').text() || '';
+          const match = description.match(/【発売日】(.*)\B/);
+          const publishAt = (match && match[1]) || 'unknown';
+          const figure: Figure = {
+            url,
+            name,
+            cover,
+            price,
+            publishAt,
+          };
+          return figure;
+        }),
+        reduce<Figure, Figure[]>((acc, it) => [...acc, it], [])
       )
       .toPromise();
   }
