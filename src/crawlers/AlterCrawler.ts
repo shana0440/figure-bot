14c6e4d08bc003e4cef924c14780ca7bc492aa06
kwguo <shana0440@gmail.com>
2020-01-29 16:20:38 +0800

    alter: fetch one by one

--- src/crawlers/AlterCrawler.ts
+++ src/crawlers/AlterCrawler.ts
@@ -1,5 +1,5 @@
-import { forkJoin, concat } from 'rxjs';
-import { mergeMap, map, reduce } from 'rxjs/operators';
+import { concat, queueScheduler } from 'rxjs';
+import { mergeMap, map, reduce, mergeAll, observeOn } from 'rxjs/operators';
 
 import { FigureCrawler } from './FigureCrawler';
 import { Request } from '../request/Request';
@@ -19,33 +19,35 @@ export class AlterCrawler implements FigureCrawler {
     for (let i = 0; i < 2; i++) {
       const year = thisYear + i;
       const request = this.request.request(`${host}/figure/?yy=${year}&mm=`).pipe(
-        mergeMap(($) => {
+        map(($) => {
           const links = $('figure a')
             .map((i, it) => host + $(it).attr('href'))
             .get();
-          return forkJoin(links.map((it) => this.request.request(it)));
+          return links;
         }),
-        map(($figures) => {
-          return $figures.map(($) => {
-            const url = host + $('#topicpath li:last-child a').attr('href');
-            const name = $('#topicpath li:last-child a').text();
-            const cover = host + $('.item-mainimg > figure > img').attr('src');
-            const price = $(
-              'div.cells:nth-child(2) > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(2)'
-            ).text();
-            const publishAt = $(
-              'div.cells:nth-child(2) > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2)'
-            ).text();
-            const figure: Figure = {
-              url,
-              name,
-              cover,
-              price,
-              publishAt,
-            };
-            return figure;
-          });
-        })
+        mergeAll<string>(),
+        mergeMap((url) => this.request.request(url)),
+        observeOn(queueScheduler),
+        map(($) => {
+          const url = host + $('#topicpath li:last-child a').attr('href');
+          const name = $('#topicpath li:last-child a').text();
+          const cover = host + $('.item-mainimg > figure > img').attr('src');
+          const price = $(
+            'div.cells:nth-child(2) > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(2)'
+          ).text();
+          const publishAt = $(
+            'div.cells:nth-child(2) > div:nth-child(1) > table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2)'
+          ).text();
+          const figure: Figure = {
+            url,
+            name,
+            cover,
+            price,
+            publishAt,
+          };
+          return figure;
+        }),
+        reduce<Figure, Figure[]>((acc, it) => [...acc, it], [])
       );
       requests.push(request);
     }
