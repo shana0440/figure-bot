5c8e05c942401342225a9ca2fbceedaa051b231c
kwguo <shana0440@gmail.com>
2021-10-16 23:01:33 +0800

    use axios replace rxjs ajax

--- src/request/Request.ts
+++ src/request/Request.ts
@@ -1,12 +1,6 @@
-import { XMLHttpRequest as NodeXMLHttpRequest } from 'xmlhttprequest-ts';
+import axios from 'axios';
 import * as Cheerio from 'cheerio';
 import { Observable } from 'rxjs';
-import { ajax } from 'rxjs/ajax';
-import { map } from 'rxjs/operators';
-
-function createXHR() {
-  return (new NodeXMLHttpRequest() as unknown) as XMLHttpRequest;
-}
 
 export interface Headers {
   [key: string]: string;
@@ -30,15 +24,20 @@ export class Response {
 
 export class Request {
   request(url: string, headers?: Headers): Observable<Response> {
-    return ajax({
-      // rxjs ajax don't support node side
-      // need to create own XMLHttpRequest
-      // but the type is using dom side XMLHttpRequest
-      // so force cast type here.
-      createXHR,
-      url,
-      method: 'GET',
-      headers,
-    }).pipe(map((resp) => new Response(resp.xhr.responseText)));
+    return new Observable((subscribe) => {
+      axios
+        .request<string>({
+          url,
+          method: 'GET',
+          headers,
+        })
+        .then((resp) => {
+          subscribe.next(new Response(resp.data));
+          subscribe.complete();
+        })
+        .catch((err) => {
+          subscribe.error(err);
+        });
+    });
   }
 }
